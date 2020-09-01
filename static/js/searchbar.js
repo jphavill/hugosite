038d59dbb067afdfc7e08ac62fceaf326a5a7c07
jphavill <jphavill@gmail.com>
2020-09-01 20:47:20 -0300

    added functional search bar with hardcoded database

--- static/js/searchbar.js
+++ static/js/searchbar.js
@@ -1,6 +1,14 @@
-// {
-//
-// }
+var searchPairs = {
+    'about': '/about',
+    'homepage': '/',
+    'if-else': '/articles/if-else',
+    'math': '/articles/math',
+    'strings': '/articles/strings',
+    'print': '/articles/print',
+    'tuples': '/articles/tuples',
+    'articles': '/articles'
+}
+
 
 function searchArticles(event, input) {
     var code = event.keyCode
@@ -8,15 +16,57 @@ function searchArticles(event, input) {
     if (code === 27) {
         // clear the serach box
         input.value=''
-        return
     }
     searchTerm = input.value.toLowerCase()
-    console.log(searchTerm)
+    let searchResultsBox = findElement('searchResults')
+    searchResultsBox.innerHTML = ''
+    if (searchTerm != ''){
+        var searchResults = []
+        for (var key in searchPairs) {
+            if (key.toLowerCase().includes(searchTerm)){
+                searchResults.push(key)
+            }
+
+        }
+
+        let resultsBox = document.createElement('ul')
+
+        for (var result in searchResults){
+            var entry = document.createElement('li')
+            var entryLink = document.createElement('a')
+            entryLink.innerText = searchResults[result]
+            entryLink.setAttribute('href', searchPairs[searchResults[result]])
+            entry.appendChild(entryLink)
+            resultsBox.appendChild(entry)
+        }
+        searchResultsBox.appendChild(resultsBox)
+    }
+
     // if the enter key is pressed
     if (code === 13) {
         // go to the top result 
 
     }
+
+
+}
+
+function findElement(identifier){
+    // returns the element using the identifier checking if it is an id, name, class and finally an element type.
+    var element = document.getElementById(identifier);
+    if (element == null){
+        element = document.getElementsByTagName(identifier)[0];
+    }
+    if (element == null){
+        element = document.getElementsByClassName(identifier)[0];
+    }
+    if (element == null){
+        element = document.getElementsByName(identifier)[0];
+    }
+    if (element == null){
+        element = document.getElementsByTagName(identifier)[0];
+    }
+    return element;
 
 }
 
